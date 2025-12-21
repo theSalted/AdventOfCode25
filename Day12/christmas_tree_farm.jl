8b0d80a4ee453e3b76e1a25c1b5ad34eafd1fb73
Yuhao <hello@chenyuhao.me>
2025-12-21 02:45:10 -0800

    wow that was hard

--- Day12/christmas_tree_farm.jl
+++ Day12/christmas_tree_farm.jl
@@ -27,35 +27,34 @@ function count_cells(shape::AbstractMatrix{<:Integer})
 end
 
 function generate_mask(width, height, shape::AbstractMatrix{<:Integer})
-    @assert width >= 3 && height >= 3
-    mask = big(0)
-    counter = 1
-
-    for y in 1:height
-        for x in 1:width
-            mask <<= 1
-
-            if y > 3 || x > 3
-                continue
+    mask = falses(width * height)
+    for sy in 1:3
+        for sx in 1:3
+            if shape[sy, sx] == 1
+                idx = (sy - 1) * width + sx
+                mask[idx] = true
             end
-
-            sy = (counter - 1) ÷ 3 + 1
-            sx = (counter - 1) % 3 + 1
-
-            mask += shape[sy, sx]
-            counter += 1
         end
     end
-
     return mask
 end
 
+function shift_mask(mask::BitVector, shift::Int)
+    if shift == 0
+        return copy(mask)
+    end
+    result = falses(length(mask))
+    result[1:end-shift] = mask[shift+1:end]
+    return result
+end
+
 function generate_masks(width, height, shape_map)
-    masks = BigInt[]
+    masks = BitVector[]
     base_mask = generate_mask(width, height, shape_map)
     for y in 1:height-2
         for x in 1:width-2
-            mask = base_mask >> ((y - 1) * width + x - 1)
+            shift = (y - 1) * width + (x - 1)
+            mask = shift_mask(base_mask, shift)
             push!(masks, mask)
         end
     end
@@ -64,7 +63,7 @@ end
 
 function generate_all_masks_for_shape(width, height, shape::AbstractMatrix{<:Integer})
     muts = unique_board_mutations(shape)
-    all_masks = Set{BigInt}()
+    all_masks = Set{BitVector}()
     for m in muts
         for mask in generate_masks(width, height, m)
             push!(all_masks, mask)
@@ -73,16 +72,43 @@ function generate_all_masks_for_shape(width, height, shape::AbstractMatrix{<:Int
     return collect(all_masks)
 end
 
-function dfs(board::BigInt, pieces_to_place::Vector{Vector{BigInt}}, piece_idx::Int)
+const MASK_CACHE = Dict{Tuple{Int,Int,Int}, Vector{BitVector}}()
+
+function get_cached_masks(width, height, shape_idx, shape)
+    key = (width, height, shape_idx)
+    if !haskey(MASK_CACHE, key)
+        MASK_CACHE[key] = generate_all_masks_for_shape(width, height, shape)
+    end
+    return MASK_CACHE[key]
+end
+
+function dfs(board::BitVector, pieces_to_place::Vector{Vector{BitVector}}, piece_idx::Int,
+             empty_cells::Int, remaining_cells_needed::Int)
     if piece_idx > length(pieces_to_place)
-        return true  # All pieces placed successfully
+        return true
+    end
+
+    if empty_cells < remaining_cells_needed
+        return false
     end
 
     masks = pieces_to_place[piece_idx]
     for mask in masks
-        if (board & mask) == 0  # No overlap
-            new_board = board | mask
-            if dfs(new_board, pieces_to_place, piece_idx + 1)
+        has_overlap = false
+        @inbounds for i in eachindex(board)
+            if board[i] && mask[i]
+                has_overlap = true
+                break
+            end
+        end
+
+        if !has_overlap
+            new_board = board .| mask
+            cells_placed = count(mask)
+            new_empty = empty_cells - cells_placed
+            new_remaining = remaining_cells_needed - cells_placed
+
+            if dfs(new_board, pieces_to_place, piece_idx + 1, new_empty, new_remaining)
                 return true
             end
         end
@@ -91,10 +117,9 @@ function dfs(board::BigInt, pieces_to_place::Vector{Vector{BigInt}}, piece_idx::
     return false
 end
 
-function can_fit_region(width, height, shapes, counts)
+function can_fit_region(width, height, shapes, counts, shape_cells)
     total_area = width * height
-
-    required_cells = sum(counts[i] * count_cells(shapes[i]) for i in 1:length(shapes))
+    required_cells = sum(counts[i] * shape_cells[i] for i in 1:length(shapes))
 
     if required_cells > total_area
         return false
@@ -104,9 +129,15 @@ function can_fit_region(width, height, shapes, counts)
         return true
     end
 
-    shape_masks = [generate_all_masks_for_shape(width, height, s) for s in shapes]
+    shape_masks = [get_cached_masks(width, height, i, shapes[i]) for i in 1:length(shapes)]
 
-    pieces_to_place = Vector{BigInt}[]
+    for i in 1:length(shapes)
+        if counts[i] > 0 && isempty(shape_masks[i])
+            return false
+        end
+    end
+
+    pieces_to_place = Vector{BitVector}[]
     for (i, count) in enumerate(counts)
         for _ in 1:count
             push!(pieces_to_place, shape_masks[i])
@@ -115,7 +146,8 @@ function can_fit_region(width, height, shapes, counts)
 
     sort!(pieces_to_place, by=length)
 
-    return dfs(big(0), pieces_to_place, 1)
+    empty_board = falses(width * height)
+    return dfs(empty_board, pieces_to_place, 1, total_area, required_cells)
 end
 
 function parse_region(line)
@@ -136,13 +168,15 @@ for block in blocks[1:6]
     push!(shapes, generate_block(rest))
 end
 
+shape_cells = [count_cells(s) for s in shapes]
+
 region_lines = split(chomp(blocks[7]), "\n")
 
 count_fits = 0
 for (i, line) in enumerate(region_lines)
     print("\r$i/$(length(region_lines))")
     w, h, counts = parse_region(line)
-    if can_fit_region(w, h, shapes, counts)
+    if can_fit_region(w, h, shapes, counts, shape_cells)
         global count_fits += 1
     end
 end
